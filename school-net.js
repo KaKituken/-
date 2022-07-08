27bd16b79d7a6177b2ed903952c5cda13ffca2ad
KaKituken <838242595@qq.com>
2022-07-08 12:07:40 +0800

    feat: re-construction

--- school-net.js
+++ school-net.js
@@ -1,9 +1,50 @@
-// const SchoolNetApp = {
-//     data() {
-//         return {
-//         message: 'Hello Vue!!'
-//         }
-//     }
-// }
-  
-// Vue.createApp(SchoolNetApp).mount('#hello-vue')
+let trafficUsed = 41
+const MAXTRAFFIC = 50
+const MAXLENGTH = 264
+let curLength = trafficUsed / MAXTRAFFIC * MAXLENGTH
+
+var user = new Vue({
+    el: '#user',
+    data:{
+        username: "Takasaki You"
+    }
+})
+
+var traffic = new Vue({
+    el: '#traffic',
+    data:{
+        traffic: String(trafficUsed) + 'G'
+    },
+})
+
+var yelloBar = new Vue({
+    el: '#traffic-value',
+    data:{
+        myStyle:{
+            width: curLength + 'px'
+        }
+    }
+})
+
+var icnow = new Date()
+var interval
+var curTime = new Vue({
+    el:'#time',
+    data:{
+        time: icnow.toTimeString().substring(0, 8)
+    },
+    created(){
+        interval = setInterval(()=>{
+            let icnow = new Date()
+            this.time = icnow.toTimeString().substring(0, 8)
+        }, 1000)
+    },
+    computed:{
+        newTime: function(){
+            return this.time
+        }
+    },
+    beforeDestroy () {
+        clearInterval(interval);
+    }
+})
